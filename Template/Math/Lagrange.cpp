41fc2faf18a56a9640ff1ab998870de0ebb9fc28
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-05-10 02:30:32 +0800

    Update Lagrange.cpp

--- Template/Math/Lagrange.cpp
+++ Template/Math/Lagrange.cpp
@@ -10,3 +10,25 @@ ll ans = 0;
 		ans+=s;
 		ans%=mod;
 	}
+
+    cin >> n >> k;
+    f[0] = 0;
+    rep(i,1,k+2){
+    	f[i]=add(f[i-1],qm(i,k));
+    }
+	if(n<k+2){
+		cout << f[n];
+		return 0;
+	}
+	fac[0]=1;
+	rep(i,1,k+3) fac[i] = mul(fac[i-1],i);
+	inv[k+2]=qm(fac[k+2],mod-2);
+	for(int i=k+1;i>=0;i--) inv[i] = mul(inv[i+1],i+1);
+	int ans = 0;
+	rep(i,0,k+2){
+		int s = mul(mul(fac[n],inv[n-k-2]),qm(n-i,mod-2));
+		s= mul(s,mul(inv[k+1-i],inv[i]));
+		if((k+1-i)%2==0)ans = add(ans,mul(f[i],s));
+		else ans = (ans-mul(f[i],s)+mod)%mod;
+   	}
+    cout << ans ;
