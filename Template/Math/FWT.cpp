9aadbc740a583fdb1d9e2289044b71d45ed18f77
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-07-09 14:17:33 +0800

    Update FWT.cpp

--- Template/Math/FWT.cpp
+++ Template/Math/FWT.cpp
@@ -1,28 +1,16 @@
-void FWT(int a[],int n)  
-{  
-    for(int d=1;d<n;d<<=1)  
-        for(int m=d<<1,i=0;i<n;i+=m)  
-            for(int j=0;j<d;j++)  
-            {  
-                int x=a[i+j],y=a[i+j+d];  
-                a[i+j]=(x+y)%mod,a[i+j+d]=(x-y+mod)%mod;  
-                //xor:a[i+j]=x+y,a[i+j+d]=(x-y+mod)%mod;  
-                //and:a[i+j]=x+y;  
-                //or:a[i+j+d]=x+y;  
-            }  
-}  
-
-void iFWT(int a[],int n)  
-{  
-    for(int d=1;d<n;d<<=1)  
-        for(int m=d<<1,i=0;i<n;i+=m)  
-            for(int j=0;j<d;j++)  
-            {  
-                int x=a[i+j],y=a[i+j+d];  
-                a[i+j]=1LL*(x+y)*rev%mod,a[i+j+d]=(1LL*(x-y)*rev%mod+mod)%mod; 
-                //rev表示2在模mod下的逆元 
-                //xor:a[i+j]=(x+y)/2,a[i+j+d]=(x-y)/2;  
-                //and:a[i+j]=x-y;  
-                //or:a[i+j+d]=y-x;  
-            }  
-}  
+void fwt(int a[],int n,int t){
+	for(int d=1;d<n;d<<=1)
+		for(int m=d<<1,i=0;i<n;i+=m)
+			for(int j=0;j<d;j++){
+				int x=a[i+j], y=a[i+j+d];
+				if(t==1){
+					// xor: a[i+j]=add(x,y), a[i+j+d]=add(x,mod-y);
+					// ans: a[i+j]=add(x,y);
+					// or:  a[i+j+d]=add(x,y);
+				}else {
+					//xor: a[i+j]=mul(add(x,y),inv2), a[i+j+d]=mul(add(x,mod-y),inv2);
+					//ans: a[i+j]=add(x,mod-y);
+					//or: a[i+j+d] = add(y,mod-x);
+				}
+			}
+}
