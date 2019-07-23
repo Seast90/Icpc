03a5e30e63865df8bb178251304487ece82e994d
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-07-23 10:44:58 +0800

    Create LinearBase.cpp

---
+++ Template/DataStructure/LinearBase.cpp
@@ -0,0 +1,28 @@
+struct LinearBase{
+    ll f[65];
+    int pos[65];
+    void insert(ll x,int y){
+        for(int i=64;~i;--i)
+        if(x>>i&1){
+            if(!f[i]){
+                f[i] = x;
+                pos[i] = y;
+                break;
+            }
+            if(pos[i] < y){
+                swap(pos[i], y);
+                swap(f[i],x);
+            }
+            x^=f[i];
+        }
+    }
+    ll query(int x){
+        ll s= 0;
+        for(int i=31;~i;--i){
+            if(f[i] && pos[i]>=x){
+                s = max(s,s^f[i]);
+            }
+        }
+        return s;
+    }
+};    
