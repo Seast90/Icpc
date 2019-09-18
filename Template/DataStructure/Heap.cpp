e7883892c9f851ac448e0722e2a12421a580f910
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-09-18 19:45:30 +0800

    Create Heap.cpp

---
+++ Template/DataStructure/Heap.cpp
@@ -0,0 +1,21 @@
+struct heap {
+	priority_queue< int > q1, q2;
+	inline void push(int x){ q1.push(x); }
+	inline void pop() { q2.push(x); }
+	inline void erase(){
+		while(q2.size() && q2.top() == q1.top()) q1.pop(), q2.pop();
+	}
+	inline int top(){
+		erase();
+		return q1.top();
+	}
+	inline int top2(){
+		int x = top();
+		q1.pop(); erase();
+		int res = top(); push(x);
+		return res;
+	}
+	inline int size(){
+		return q1.size() - q2.size();
+	}
+}
