33b4a0bcb95bbcea9996a02696555c40d9941c91
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-09-18 19:37:00 +0800

    Update 点分树.cpp

--- Template/Tree/点分树.cpp
+++ Template/Tree/点分树.cpp
@@ -1,24 +1,22 @@
 // id starts from 1
-namespace Centriod {
-	const int N = ::N;
-	bool vis[N]; int sz[N], par[N]; vi G[N];
-	void dfssz(int c,int fa,int Sz,int &rt){
-		sz[c] = 1;
-		for(auto t : g[c]) if(!vis[t]&&t!=fa) dfssz(t,c,Sz,rt) , sz[c]+=sz[t];
-		if(!rt && sz[c]*2>Sz) rt=c;
+namespace Centriod{
+	const int N=101010<<1;
+	bool vis[N];
+	int sz[N], fa[N]; vi e[N];
+	void dfssz(int x,int p,int Sz,int &rt){
+		sz[x] = 1;
+		for(auto u:e[x]) if(!vis[u] && u!=p) dfssz(u, x, Sz, rt), sz[x]+=sz[u];
+		if(!rt && sz[x]*2 > Sz) rt = x;
 	}
-	int dfs(int c){
-		int rt=0;dfssz(c,0,0,rt);dfssz(c,0,sz[c],rt=0);
-		vis[rt] = true;
-		for(auto v : g[rt]) if(!vis[v]) {
-			int t = dfs(v);
-			G[rt].pb(t);
-			par[t] = rt;
+	int dfs(int x){
+		int rt = 0;
+		dfssz(x,0,0,rt); dfssz(x, 0,sz[x], rt=0);
+		vis[rt] = 1;
+		for(auto u:e[rt]) if(!vis[u]){
+			int v = dfs(u);
+			e[rt].pb(v);
+			fa[v] = rt;
 		}
 		return rt;
-	}
-	void init() { 
-		fill_n(G + 1, n, vi()); 
-		fill_n(par + 1, n, 0);
 	}
 };
