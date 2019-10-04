1c771ada014d40f3993bd0a98ea8d23aad06c2f7
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-10-04 19:39:59 +0800

    Update Dsu_on_tree.cpp

--- Template/Tree/Dsu_on_tree.cpp
+++ Template/Tree/Dsu_on_tree.cpp
@@ -9,11 +9,11 @@ struct Dsuontree{
   void dfs(int x,int p,int ty){
     for(auto u:v[x]){
       if(u==son[x] || u==p) continue;
-      dfs(u,x,0);
+      dfs(u,x,0); //暴力统计轻边的贡献，ty= 0表示递归完成后消除对该点的影响
     }
-    if(son[x]) dfs(son[x],x,1), Son=son[x];
-    add(x,p,1);
-    ans[x] = sum;   Son=0;
-    if(!ty) add(x,p,-1), sum=0,mx=0;
+    if(son[x]) dfs(son[x],x,1), Son=son[x]; //统计重儿子的贡献，不消除影响
+    add(x,p,1); 
+    ans[x] = sum;   Son=0;//暴力统计所有轻儿子的贡献
+    if(!ty) add(x,p,-1), sum=0,mx=0; //如果需要删除贡献的话就删掉
   }
 }
