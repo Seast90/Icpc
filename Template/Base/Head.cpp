c84fe54c7a028f3a609007e1b072941d88bf9f8b
Seast90 <32071792+Seast90@users.noreply.github.com>
2019-12-07 21:28:56 +0800

    Create Head.cpp

---
+++ Template/Base/Head.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define per(i,a,b) for(int i=(b)-1;i>=a;i--)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define de(x) cout<<#x<<" = "<<x<<"\n"
+#define dd(x) cout<<#x<<" = "<<x<<" "
+#define mes(p) memset(p,0,sizeof(p))
+#define all(x) x.begin(),x.end()
+#define rson mid+1,r,rt<<1|1
+#define sz(x) (int)x.size()
+#define lson l,mid,rt<<1
+#define mp make_pair 
+#define pb push_back
+#define ls (rt<<1)
+#define rs (ls|1)
+#define se second
+#define fi first
+typedef pair <int ,int > pii;
+typedef vector <int > vi;
+typedef long long ll;
+typedef double db;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f; 
+const int mod = 1e9+7;
+
+const int maxn=2e5+5;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+	return 0; 
+}
