a74ac8745217bf0c67ff69cda496eaf4981dada7
Seast90 <32071792+Seast90@users.noreply.github.com>
2020-03-29 03:52:03 +0800

    Update Lca.cpp

--- Template/Tree/Lca.cpp
+++ Template/Tree/Lca.cpp
@@ -1,18 +1,18 @@
 void dfs(int x,int p,int d){ // dfs(rt,rt,0)
 	dep[x] = d;
 	f[x][0] = p;
-	rep(i,1,30)
+	rep(i,1,22)
 		f[x][i] = f[f[x][i-1]][i-1];
-	for(auto u:v[x])
+	for(auto u:e[x])
 		if(u!=p)
 			dfs(u,x,d+1);
 }
 int lca(int u,int v){
 	if(dep[u] < dep[v]) swap(u,v);
-	for(int i=29,d=dep[u]-dep[v];i>=0;i--)
+	for(int i=22,d=dep[u]-dep[v];i>=0;i--)
 		if(d>>i&1) u=f[u][i];
 	if(u==v) return v;
-	for(int i=29;i>=0;i--)
+	for(int i=22;i>=0;i--)
 	if(f[u][i]!=f[v][i])
 		u=f[u][i], v=f[v][i];
 	return f[u][0];
