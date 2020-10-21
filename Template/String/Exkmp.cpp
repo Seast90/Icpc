a637b8f053067bdb544a4c077be4fcd325e42254
Seast90 <32071792+Seast90@users.noreply.github.com>
2020-10-21 22:29:59 +0800

    Update Exkmp.cpp

--- Template/String/Exkmp.cpp
+++ Template/String/Exkmp.cpp
@@ -13,3 +13,41 @@ void zfunction() {
 		if(i + z[i] - 1 > R) R = i + z[i] - 1,L = i;
 	}
 }
+/*
+< 准备 >
+    1. 字符串 s[] 与字符串 t[] ，下标从 0 开始
+< 使用 >
+    1. 调用 ex.exkmp(s, n, t, m) ， n 和 m 分别为 s[] 与 t[] 的长度，得
+    .. lcp[i] 表示 t[] 与 s[i, n-1] 的最长公共前缀长度
+    .. net[i] 表示 t[] 与 t[i, m-1] 的最长公共前缀长度
+*/
+
+int net[N], lcp[N];
+struct Extend_KMP {
+    void Get(char t[], int m) {
+        int a = 0, p = 0;
+        net[0] = m;
+        rep(i, 1, m) {
+            if (i >= p || i + net[i-a] >= p) {
+                if (i >= p) p = i;
+                while (p < m && t[p] == t[p-i]) p++;
+                net[a=i] = p - i;
+            } else {
+                net[i] = net[i-a];
+            }
+        }
+    }
+    void exkmp(char s[], int n, char t[], int m) {
+        Get(t, m);
+        int a = 0, p = 0;
+        rep(i, 0, n) {
+            if (i >= p || i + net[i-a] >= p) {
+                if (i >= p) p = i;
+                while (p < n && p - i < m && s[p] == t[p-i]) p++;
+                lcp[a=i] = p - i;
+            } else {
+                lcp[i] = net[i-a];
+            }
+        }
+    }
+} ex;
