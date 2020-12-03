6f41791e0f8c6b43264971d203d02f9ac4de059f
Seast90 <32071792+Seast90@users.noreply.github.com>
2020-12-03 21:00:39 +0800

    Update LongChain.cpp

--- Template/Tree/LongChain.cpp
+++ Template/Tree/LongChain.cpp
@@ -59,3 +59,21 @@ struct LongChain{
 		find();
 	}
 };
+int len[N],son[N],tmp[N],*f[N],*id=tmp,ans[N];
+void dp(int u,int ff){
+	f[u][0]=1;
+	if (son[u]) f[son[u]]=f[u]+1,dp(son[u],u),ans[u]=ans[son[u]]+1;
+	for (int e=head[u];e;e=nxt[e]){
+		int v=to[e];if (v==ff||v==son[u]) continue;
+		f[v]=id;id+=len[v];dp(v,u);
+		for (int j=1;j<=len[v];++j){
+			f[u][j]+=f[v][j-1];
+			if ((j<ans[u]&&f[u][j]>=f[u][ans[u]])||(j>ans[u]&&f[u][j]>f[u][ans[u]]))
+				ans[u]=j;
+		}
+	}
+	if (f[u][ans[u]]==1) ans[u]=0;
+}
+dfs(1,0);
+f[1]=id; id+=len[1];
+dp(1,0);
