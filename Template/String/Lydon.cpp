dbe90d83bc6606a7a6072dc51ae221759de68a17
Seast90 <32071792+Seast90@users.noreply.github.com>
2021-02-17 03:05:39 +0800

    Update Lydon.cpp

--- Template/String/Lydon.cpp
+++ Template/String/Lydon.cpp
@@ -1,19 +1,15 @@
 vector<int>Pos;
 void deal(char *x, int n) {
     int i = 0;
-
     while (i < n) {
         int j = i, k = i + 1;
-
         while (k < n && x[j] <= x[k]) {
             if (x[j] == x[k])
                 j++;
             else
                 j = i;
-
             k++;
         }
-
         while (i <= j) {
             i += k - j;
             Pos.push_back(i);
